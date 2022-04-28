b18d87b2bf7019772b899f14d8617dbb54306859
eldersol <eldertojins@gmail.com>
2022-04-28 17:47:33 -0600

    [ADD]Agregar Tarea 2

---
+++ Tarea2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import csv
+import os
+
+
+with open('data_ids.csv') as archivo:
+    contenido = csv.reader(archivo, delimiter=',', quotechar=' ')
+    archivo2 = open("Archivo2.sql","w") 
+    lista = []
+    ids = ""
+
+    for linea in contenido:
+        lista.append(linea[0])
+    
+
+    
+    for grupo in lista[:99]:
+        ids += " ID = " + str(grupo) + " or " 
+    
+    ids += " ID = " + str(lista[100])
+
+    archivo2.write("UPDATE suscripcion SET estado = false WHERE " + str(ids) + ";\n")     
+    archivo2.close() 
+
+
