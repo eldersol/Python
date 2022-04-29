02b6e34f4965dc53da15fe53e230b1011932e137
eldersol <eldertojins@gmail.com>
2022-04-29 12:55:22 -0600

    [FIX]Solucion de for anidado en Tarea2.py

--- Tarea2.py
+++ Tarea2.py
@@ -7,19 +7,24 @@ with open('data_ids.csv') as archivo:
     contenido = csv.reader(archivo, delimiter=',', quotechar=' ')
     archivo2 = open("Archivo2.sql","w") 
     lista = []
-    ids = ""
+    
 
     for linea in contenido:
         lista.append(linea[0])
     
-    for a in range(len(lista)/100) :
+    for x in range(len(lista)/100):
+        ids = ""
+        g100=[]
+
+        for grupo in lista[x*100:(x*100)+100]:
+            g100.append(grupo)
         
-        for grupo in lista[:99]:
-            ids += " ID = " + str(grupo) + " or " 
-
-        ids += " ID = " + str(lista[100])
-
+        for id in g100:
+            ids += " ID = " + str(id) + " or " 
+        
+        ids += " ID = " + str(g100[99])        
         archivo2.write("UPDATE suscripcion SET estado = false WHERE " + str(ids) + ";\n")     
+    
     archivo2.close() 
 
 
